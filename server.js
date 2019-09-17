41d6a9d3c7d364aa72d8cda1e63f0bb7701397de
yaelBrown <yaelrbrown@gmail.com>
2019-09-17 12:06:50 -0500

    Able to connect to mlab mongo database.

--- server.js
+++ server.js
@@ -7,12 +7,29 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://admin:password123@ds311968.mlab.com:11968/git-connected", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("connected to database");
+  }
+});
+
+let loginSchema = new mongoose.Schema({
+  name: String,
+  location: String,
+  favoriteColor: String
+});
+
+let login = mongoose.model("login", loginSchema);
 
 server.use('/api', router);
 
-
-// app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-
-app.use('/', app.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
